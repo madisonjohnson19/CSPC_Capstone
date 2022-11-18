22bf437fea5b3160fe27e25485a23aa281a8ae01
Madison Johnson <madison.johnson.19@cnu.edu>
2022-11-18 17:58:28 -0500

    github pages

--- cnu-security/Server/index.js
+++ cnu-security/Server/index.js
@@ -46,10 +46,28 @@ app.get('/api/get/report/resolve',(req,res)=>{
 app.get('/api/get/users/:cnuID',(req,res)=>{
     let cnuID = req.params.cnuID;
     // let cnuID = "12345";
-    let sql = `SELECT * FROM student WHERE CNUID= "${cnuID}";`
+    
+    // let sql = `(SELECT * FROM student WHERE CNUID= "${cnuID}");`
+    let sql = `SELECT distinct *,count(a.CNUID)
+    FROM student s, assistancerequest a
+    WHERE  a.CNUID= "${cnuID}" and a.CNUID= s.cnuid;`
     db.query(sql,(err,result)=>{
         res.send(result);
         console.log("GET STUDENT: ",result[0]["firstName"]+ " CNUID: "+sql)
+    });
+})
+app.get('/api/get/checkStudExists/:cnuID',(req,res)=>{
+    let cnuID = req.params.cnuID;
+    // let cnuID = "12345";
+    let sql = `SELECT EXISTS (SELECT * FROM student WHERE cnuID= "${cnuID}") AS "exists";`
+    db.query(sql,(err,result)=>{
+        let r =Object.values(JSON.parse(JSON.stringify(result)))
+        // let h =r[0][ 'EXISTS (SELECT * FROM student WHERE cnuID= "12345")']
+        let string_=(r[0]["exists"])  
+        res.send(r[0]);
+        
+        console.log(r[0] ," : "+ cnuID);
+        // console.log("Exists?: ",r[0][ 'EXISTS (SELECT * FROM student WHERE cnuID= "12345")']);
     });
 })
 
@@ -69,7 +87,7 @@ app.put('/api/update/addCNUID/maxID',(req,res)=>{
     let cnuID = req.body.cnuID;
     let lastID = req.body.lastID;
     console.log("Update to send cnuID: "+cnuID+ "  maxID: "+lastID)
-    const sqlSelect= `UPDATE assistancerequest SET CNUID = ${cnuID} WHERE aRID =${lastID};`;
+    const sqlSelect= ` UPDATE assistancerequest SET CNUID = ${cnuID} WHERE aRID =${lastID};`;
     db.query(sqlSelect,(err,result)=>{
         // let hold =result[0]["max(aRID)"]+""
         res.send(result);
@@ -121,7 +139,7 @@ app.post('/api/insert/test', (req, res)=>{
 })
 app.post('/api/insert/reportCrime', (req, res)=>{
     console.log('report added: '+req.body.aRID)
-    let post = {dateTime:req.body.dateTime ,location: req.body.location}
+    let post = {aRID:req.body.lastID,dateTime:req.body.dateTime ,location: req.body.location}
     let sql = 'INSERT INTO assistancerequest SET ?'
     let query = db.query(sql, post, err =>{
         if (err){
